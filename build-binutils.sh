0125ba4a7b624ac5ea4561c4d43c6807508a3d43
qethu <190734095+qethu@users.noreply.github.com>
2025-12-17 20:52:41 +0800

    Workarond gas ftbfs

--- build-binutils.sh
+++ build-binutils.sh
@@ -4,7 +4,7 @@ CWD=`pwd`
 cd $CWD/binutils-gdb
 rm -rf build && mkdir build
 cd build
-CFLAGS="-g -O3 -Wno-strict-prototypes -Wno-sign-compare" \
+CFLAGS="-g -O3 -Wno-strict-prototypes -Wno-sign-compare -Wno-format-truncation" \
    ../configure --target=mipsel-mti-elf --prefix=$CWD/binutils-gdb/build/dest --disable-gdb --disable-sim
 make -j
 make install
